4fe48f1364e9f2b2685321b2aca18844c2c54d83
chut-ai <louis.hemadou@eleves.enpc.fr>
2021-03-11 16:37:14 +0100

    Bug to fix

--- basic_dann/models.py
+++ basic_dann/models.py
@@ -1,59 +1,46 @@
 import torch.nn as nn
-from dann.transfer import get_loader
 from dann.basic_dann.function import ReverseLayerF
 
-class Encoder(nn.Module):
+
+class CNNModel(nn.Module):
 
     def __init__(self):
-        super(Encoder, self).__init__()
-        self.net = nn.Sequential()
-        self.net.add_module("conv1", nn.Conv2d(1, 32, kernel_size=5))
-        self.net.add_module("bn1", nn.BatchNorm2d(32))
-        self.net.add_module("pool1", nn.MaxPool2d(2))
-        self.net.add_module("relu1", nn.ReLU(True))
-        self.net.add_module("conv2", nn.Conv2d(32, 64, kernel_size=5))
-        self.net.add_module("bn2", nn.BatchNorm2d(64))
-        self.net.add_module("pool2", nn.MaxPool2d(2))
-        self.net.add_module("relu2", nn.ReLU(True))
-        self.net.add_module("drop1", nn.Dropout2d())
+        super(CNNModel, self).__init__()
+        self.feature = nn.Sequential()
+        self.feature.add_module('f_conv1', nn.Conv2d(1, 64, kernel_size=5))
+        self.feature.add_module('f_bn1', nn.BatchNorm2d(64))
+        self.feature.add_module('f_pool1', nn.MaxPool2d(2))
+        self.feature.add_module('f_relu1', nn.ReLU(True))
+        self.feature.add_module('f_conv2', nn.Conv2d(64, 50, kernel_size=5))
+        self.feature.add_module('f_bn2', nn.BatchNorm2d(50))
+        self.feature.add_module('f_drop1', nn.Dropout2d())
+        self.feature.add_module('f_pool2', nn.MaxPool2d(2))
+        self.feature.add_module('f_relu2', nn.ReLU(True))
 
-    def forward(self, x):
-        x = self.net(x)
-        x = x.view(-1, 64*4*4)
-        return x
+        self.class_classifier = nn.Sequential()
+        self.class_classifier.add_module('c_fc1', nn.Linear(50 * 4 * 4, 100))
+        self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(100))
+        self.class_classifier.add_module('c_relu1', nn.ReLU(True))
+        self.class_classifier.add_module('c_drop1', nn.Dropout2d())
+        self.class_classifier.add_module('c_fc2', nn.Linear(100, 100))
+        self.class_classifier.add_module('c_bn2', nn.BatchNorm1d(100))
+        self.class_classifier.add_module('c_relu2', nn.ReLU(True))
+        self.class_classifier.add_module('c_fc3', nn.Linear(100, 10))
+        self.class_classifier.add_module('c_softmax', nn.LogSoftmax())
 
-class Classifier(nn.Module):
+        self.domain_classifier = nn.Sequential()
+        self.domain_classifier.add_module('d_fc1', nn.Linear(50 * 4 * 4, 100))
+        self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(100))
+        self.domain_classifier.add_module('d_relu1', nn.ReLU(True))
+        self.domain_classifier.add_module('d_fc2', nn.Linear(100, 2))
+        self.domain_classifier.add_module('d_softmax', nn.LogSoftmax(dim=1))
 
-    def __init__(self):
-        super(Classifier, self).__init__()
-        self.net = nn.Sequential()
-        self.net.add_module("fc1", nn.Linear(64*4*4, 500))
-        self.net.add_module("bn1", nn.BatchNorm1d(500))
-        self.net.add_module("relu1", nn.ReLU(True))
-        self.net.add_module("drop1", nn.Dropout2d())
-        self.net.add_module("fc2", nn.Linear(500, 100))
-        self.net.add_module("bn2", nn.BatchNorm1d(100))
-        self.net.add_module("relu2", nn.ReLU(True))
-        self.net.add_module("fc3", nn.Linear(100, 10))
-        self.net.add_module("softmax", nn.Softmax(dim=1))
+    def forward(self, input_data, alpha):
+        input_data = input_data.expand(input_data.data.shape[0], 1, 28, 28)
+        feature = self.feature(input_data)
+        feature = feature.view(-1, 50 * 4 * 4)
+        reverse_feature = ReverseLayerF.apply(feature, alpha)
+        class_output = self.class_classifier(feature)
+        domain_output = self.domain_classifier(reverse_feature)
 
-    def forward(self, x):
-        x = self.net(x)
-        return x
-
-
-class Discriminator(nn.Module):
-
-    def __init__(self):
-        super(Discriminator, self).__init__()
-        self.net = nn.Sequential()
-        self.net.add_module("fc1", nn.Linear(64*4*4, 100))
-        self.net.add_module("bn1", nn.BatchNorm1d(100))
-        self.net.add_module("relu1", nn.ReLU(True))
-        self.net.add_module("fc2", nn.Linear(100, 2))
-        self.net.add_module("softmax", nn.Softmax(dim=1))
-
-    def forward(self, x, alpha):
-        x = ReverseLayerF.apply(x, alpha)
-        x = self.net(x)
-        return x
+        return class_output, domain_output
--- basic_dann/train.py
+++ basic_dann/train.py
@@ -1,113 +1,78 @@
 from dann.transfer.get_loader import get_mnist, get_svhn
-from dann.basic_dann.models import Discriminator, Encoder, Classifier
+from dann.basic_dann.models import CNNModel
 import torch.optim as optim
 import torch
 import numpy as np
 from torch.autograd import Variable
 
 batch_size = 64
+lr = 1e-3
+n_epoch = 1
 
 source_train, source_val = get_svhn(batch_size)
 target_train, target_val = get_mnist(batch_size)
 
+DANN = CNNModel().cuda()
 
-class DANN():
-    def __init__(self):
+optimizer = optim.Adam(DANN.parameters(), lr)
 
-        self.encoder = Encoder().cuda()
-        self.classifier = Classifier().cuda()
-        self.discriminator = Discriminator().cuda()
+loss_class = torch.nn.NLLLoss().cuda()
+loss_domain = torch.nn.NLLLoss().cuda()
 
-        parameters = list(self.encoder.parameters(
-        )) + list(self.classifier.parameters()) + list(self.discriminator.parameters())
+for p in DANN.parameters():
+    p.requires_grad = True
 
-        self.optimizer = optim.Adam(parameters, 1e-3)
+for epoch in range(n_epoch):
 
-        self.loss_class = torch.nn.NLLLoss().cuda()
-        self.loss_domain = torch.nn.NLLLoss().cuda()
+    len_dataloader = len(source_train)
 
-    def train(self, n_epoch, source_train, target_train):
-        """Source_train and target_train must have the same dimension"""
+    for i, (source_data, target_data) in enumerate(zip(source_train, target_train)):
 
-        for epoch in range(n_epoch):
+        if len(source_data) != len(target_data):
+            break
 
-            correct_source = 0
-            correct_target = 0
+        p = float(i + epoch * len_dataloader) / (n_epoch * len_dataloader)
+        alpha = 2. / (1. + np.exp(-10 * p)) - 1
 
-            for i, (source_data, target_data) in enumerate(zip(source_train, target_train)):
+        source_inputs, source_labels = source_data
+        source_inputs = source_inputs.cuda()
+        source_labels = source_labels.cuda()
 
-                if i == len(source_train)-1:
-                    break
+        target_inputs, target_labels = target_data
+        # Target labels just for evaluating model
+        target_inputs = target_inputs.cuda()
+        target_labels = target_labels.cuda()
 
-                p = float(i + epoch*len(source_data)) / \
-                    (n_epoch * len(source_data))
-                alpha = 2. / (1. + np.exp(-10 * p)) - 1
+        DANN.zero_grad()
+        batch_size = len(source_data)
 
-                # Training NN using source data
+        # Train model using source data
 
-                source_inputs, source_labels = source_data
-                source_inputs = source_inputs.cuda()
-                source_labels = source_labels.cuda()
+        domain_label = torch.zeros(batch_size).long().cuda()
 
-                target_inputs, target_labels = target_data
-                # Target labels just for evaluating model
-                target_inputs = target_inputs.cuda()
-                target_labels = target_labels.cuda()
+        class_output, domain_output = DANN(source_inputs, alpha)
+        err_s_label = loss_class(class_output, source_labels)
+        err_s_domain = loss_domain(domain_output, domain_label)
 
-                self.optimizer.zero_grad()
-                batch_size = len(source_labels)
+        # Train model using target data
 
-                source_domain_label = torch.zeros(batch_size).long().cuda()
+        domain_label = torch.ones(batch_size).long().cuda()
 
-                encoded = self.encoder(source_inputs)
-                source_class_output = self.classifier(encoded)
-                source_domain_output = self.discriminator(encoded, alpha)
+        _, domain_output = DANN(target_inputs, alpha)
+        err_t_domain = loss_domain(domain_output, domain_label)
 
-                err_source_label = self.loss_class(
-                    source_class_output, source_labels)
-                err_source_domain = self.loss_domain(
-                    source_domain_output, source_domain_label)
+        err = err_t_domain + err_s_domain + err_s_label
 
-                # Training model using target data
+        err.backward()
+        optimizer.step()
 
-                target_domain_label = torch.ones(batch_size).long().cuda()
-
-                encoded = self.encoder(target_inputs)
-                target_class_output = self.classifier(encoded)
-                target_domain_output = self.discriminator(encoded, alpha)
-
-                err_target_domain = self.loss_domain(
-                    target_domain_output, target_domain_label)
-                err = err_target_domain + err_source_label + err_source_domain
-                # err = err_source_label
-                err.backward()
-                self.optimizer.step()
-
-                # Get predicted values for source
-                predicted_source = torch.max(source_class_output, 1)[1]
-                correct_source += (predicted_source == source_labels).sum()
-                correct_percent_source = correct_source*100/(batch_size*(i+1))
-
-                # Get predicted values for target
-                predicted_target = torch.max(target_class_output, 1)[1]
-                correct_target += (predicted_target == target_labels).sum()
-                correct_percent_target = correct_target*100/(batch_size*(i+1))
-
-                if i % 50 == 0:
-                    message = "Epochs : {}/{}, ({:.0f}%), Source accuracy:{:.3f}, Target accuracy:{:.3f}, Err source domain:{:.3f}, Err target domain:{:.3f}".format(
-                        epoch+1, n_epoch, 100*i/len(source_train), correct_percent_source, correct_percent_target, err_source_domain, err_target_domain)
-                    print(message)
+        if i % 50 == 0:
+            message = "Epochs : {}/{}, ({:.0f}%), Err source domain:{:.3f}, Err source label:{:.3f}, Err target domain:{:.3f}".format(
+                epoch+1, n_epoch, 100*i/len(source_train), err_s_domain, err_s_label, err_t_domain)
+            print(message)
 
 
-dann = DANN()
-dann.train(15, source_train, target_train)
-
-
-# Evaluate model
-
-dann.encoder.eval()
-dann.classifier.eval()
-
+"""
 with torch.no_grad():
     correct_source = 0
     for val_source_imgs, val_source_labels in source_val:
@@ -127,3 +92,4 @@ with torch.no_grad():
         correct_target += (predicted == val_target_labels).sum()
     correct_target_percent = 100*correct_target/(len(target_val)*batch_size)
     print(correct_target_percent)
+"""
