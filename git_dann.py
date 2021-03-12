43af2ae063e49dd2069fa8cda3f365c764c143ba
chut-ai <louis.hemadou@eleves.enpc.fr>
2021-03-12 13:57:32 +0100

    hehe xd

---
+++ git_dann.py
@@ -0,0 +1,196 @@
+import torch
+import torchvision
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+import numpy as np
+import datetime
+import os, sys
+from matplotlib.pyplot import imshow, imsave
+from dann.transfer.get_loader import get_mnist, get_svhn
+
+MODEL_NAME = 'DANN'
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+
+class FeatureExtractor(nn.Module):
+    """
+        Feature Extractor
+    """
+    def __init__(self, in_channel=1, hidden_dims=512):
+        super(FeatureExtractor, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channel, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, hidden_dims, 3, padding=1),
+            nn.BatchNorm2d(hidden_dims),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1,1)),
+        )
+
+    def forward(self, x):
+        h = self.conv(x).squeeze() # (N, hidden_dims)
+        return h
+
+class Classifier(nn.Module):
+    """
+        Classifier
+    """
+    def __init__(self, input_size=512, num_classes=10):
+        super(Classifier, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Linear(input_size, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, num_classes),
+        )
+
+    def forward(self, h):
+        c = self.layer(h)
+        return c
+
+
+
+class Discriminator(nn.Module):
+    """
+        Simple Discriminator w/ MLP
+    """
+    def __init__(self, input_size=512, num_classes=1):
+        super(Discriminator, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Linear(input_size, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 128),
+            nn.LeakyReLU(0.2),
+            nn.Linear(128, num_classes),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, h):
+        y = self.layer(h)
+        return y
+
+F = FeatureExtractor().to(DEVICE)
+C = Classifier().to(DEVICE)
+D = Discriminator().to(DEVICE)
+
+batch_size = 64
+mnist_loader, test_loader = get_mnist(batch_size)
+svhn_loader, eval_loader = get_svhn(batch_size)
+
+bce = nn.BCELoss()
+xe = nn.CrossEntropyLoss()
+
+F_opt = torch.optim.Adam(F.parameters())
+C_opt = torch.optim.Adam(C.parameters())
+D_opt = torch.optim.Adam(D.parameters())
+
+max_epoch = 50
+step = 0
+n_critic = 1 # for training more k steps about Discriminator
+n_batches = len(mnist_loader)
+# lamda = 0.01
+
+D_src = torch.ones(batch_size, 1).to(DEVICE) # Discriminator Label to real
+D_tgt = torch.zeros(batch_size, 1).to(DEVICE) # Discriminator Label to fake
+D_labels = torch.cat([D_src, D_tgt], dim=0)
+
+
+
+def get_lambda(epoch, max_epoch):
+    p = epoch / max_epoch
+    return 2. / (1+np.exp(-10.*p)) - 1.
+
+
+
+
+mnist_set = iter(mnist_loader)
+
+def sample_mnist(step, n_batches):
+    global mnist_set
+    if step % n_batches == 0:
+        mnist_set = iter(mnist_loader)
+    return mnist_set.next()
+
+
+ll_c, ll_d = [], []
+acc_lst = []
+
+for epoch in range(1, max_epoch+1):
+    for idx, (src_images, labels) in enumerate(svhn_loader):
+        tgt_images, _ = sample_mnist(step, n_batches)
+        # Training Discriminator
+        src, labels, tgt = src_images.to(DEVICE), labels.to(DEVICE), tgt_images.to(DEVICE)
+
+        x = torch.cat([src, tgt], dim=0)
+        h = F(x)
+        y = D(h.detach())
+
+        Ld = bce(y, D_labels)
+        D.zero_grad()
+        Ld.backward()
+        #D_opt.step()
+
+
+        c = C(h[:batch_size])
+        y = D(h)
+        Lc = xe(c, labels)
+        Ld = bce(y, D_labels)
+        lamda = 0.1*get_lambda(epoch, max_epoch)
+        Ltot = Lc -lamda*Ld
+        Ltot = Lc
+
+
+        F.zero_grad()
+        C.zero_grad()
+        D.zero_grad()
+
+        Ltot.backward()
+
+        C_opt.step()
+        F_opt.step()
+
+        if step % 100 == 0:
+            dt = datetime.datetime.now().strftime('%H:%M:%S')
+            print('Epoch: {}/{}, Step: {}, D Loss: {:.4f}, C Loss: {:.4f}, lambda: {:.4f} ---- {}'.format(epoch, max_epoch, step, Ld.item(), Lc.item(), lamda, dt))
+            ll_c.append(Lc)
+            ll_d.append(Ld)
+
+        if step % 500 == 0:
+            F.eval()
+            C.eval()
+            with torch.no_grad():
+                corrects = torch.zeros(1).to(DEVICE)
+                for idx, (src, labels) in enumerate(eval_loader):
+                    src, labels = src.to(DEVICE), labels.to(DEVICE)
+                    c = C(F(src))
+                    _, preds = torch.max(c, 1)
+                    corrects += (preds == labels).sum()
+                acc = corrects.item() / len(eval_loader.dataset)
+                print('***** Eval Result: {:.4f}, Step: {}'.format(acc, step))
+
+                corrects = torch.zeros(1).to(DEVICE)
+                for idx, (tgt, labels) in enumerate(test_loader):
+                    tgt, labels = tgt.to(DEVICE), labels.to(DEVICE)
+                    c = C(F(tgt))
+                    _, preds = torch.max(c, 1)
+                    corrects += (preds == labels).sum()
+                acc = corrects.item() / len(test_loader.dataset)
+                print('***** Test Result: {:.4f}, Step: {}'.format(acc, step))
+                acc_lst.append(acc)
+
+            F.train()
+            C.train()
+        step += 1
+
